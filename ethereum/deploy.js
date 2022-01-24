54898552b7e346204082510f1a6dbae77a33081c
Austin White <austinlldb@gmail.com>
2022-01-23 18:59:10 -0700

    infrastructure

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,8 +3,8 @@ const Web3 = require("web3");
 const compiledFactory = require("../ethereum/build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
-  "YOUR METAMASK PHRASE",
-  "BLOCKCHAIN NODE ADDRESS"
+  "YOUR WALLET PHRASE",
+  "https://rinkeby.infura.io/v3/cb05a8b0617446b4a47d925d33acc41f"
 );
 const web3 = new Web3(provider);
 
